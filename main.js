a2168f42739197dda9ef4e50f9636b7f9eb1740f
Emily <emilyrachelstickler@gmail.com>
2022-08-01 17:17:59 +0200

    show url values on confirmation

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 var params = (new URL(document.location)).searchParams;
 let category = params.get('category');
-let allergens = params.get('allergens'); //only shows first...
-let allergens2 = params.get('')
+let allergens = params.getAll('allergens');
 let vegStatus = params.get('veg-status');
 
-
 console.log(category,allergens,vegStatus);
+
+const catCell = document.getElementById("cat");
+const alleCell = document.getElementById("alle");
+const vegCell = document.getElementById("veg");
+
+catCell.innerHTML = category;
+alleCell.innerHTML = allergens;
+vegCell.innerHTML = vegStatus;
